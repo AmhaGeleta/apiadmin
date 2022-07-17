b7a2e7bf69ffc63c701e26dd88f504b72446169a
Amha Geleta <amha.nurilligne@gmail.com>
2022-07-17 18:50:19 -0500

    apiadmin

---
+++ admin/src/pages/single/Single.jsx
@@ -0,0 +1,73 @@
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import axios from "axios";
+
+const Single = () => {
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+  const id = location.pathname.split("/")[2];
+//  const f =  data.map(d => {if(d._id==id)return d});
+// console.log(id);
+  const { data, loading, error } = useFetch(`/${path}`);
+  console.log(data);
+ const dat = data.filter(d => d._id==id);
+ const firstElt = dat[0];
+ console.log("first Element", data);
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/lamadev/image/upload/v1658091624/upload/ww3ggi3dwakenusejmaz.jpg"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">{firstElt.username}</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">{firstElt.email}</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">{firstElt.phone}</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">City:</span>
+                  <span className="itemValue">
+                    {firstElt.city + `,`+firstElt.country}
+                  </span>
+                </div>
+                <div className="detailI tem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+          </div>
+        </div>
+        <div className="bottom">
+        <h1 className="title">Last Transactions</h1>
+          <List/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Single;
---
+++ index.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const app = express();
+dotenv.config();
+// const path = require('path');
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to mongoDB.');
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('mongoDB disconnected!');
+});
+
+//middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+
+// app.use(express.static(path.join(__dirname, '/client2/build/')));
+app.use(express.static(path.resolve(__dirname, './client2/build')));
+
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './client2/build', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || 'Something went wrong!';
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(process.env.PORT || 5002, function () {
+  connect();
+  console.log(
+    'Express server listening on port %d in %s mode',
+    this.address().port,
+    app.settings.env
+  );
+});
